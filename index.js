664af650ecf5227082e678d2f0cbca96e6d3dfa4
Manish Devrani <79859472+manish591@users.noreply.github.com>
2021-07-13 17:46:26 +0000

    added test for leap year

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const readLineSync = require('readline-sync');
-const chalk = reauire('chalk');
+const chalk = require('chalk');
 
 let username = readLineSync.question('What is your name? ');
-console.log(`Welcome! ${username} in this game of leap year. So, let's begin the game.`)
+console.log(`Welcome! ${username} in this game of leap year.`)
+console.log('                   ');
+if (readLineSync.keyInYN('Do you know the rules?')) {
+  console.log('Ok! lets begin.')
+} else {
+  console.log('You have to input your birthyear and i will tell you based on certain calculation that whether this year is leap year or not.')
+}
+console.log('                   ');
 
-let rules = readLineSync.keyInSelect
+let birthYear = readLineSync.question('Please input you birthyear in YYYY format!');
+
+if (dateOfBirth.length !== 4) {
+  return console.log(`Hey! ${username}, You didn't enter birth year properly, now refresh to play it again.`);
+}
+
+
+let formatYearInNumber = parseInt(birthyear);
+function isLeap (year) {
+  if(year % 4 === 0) {
+    if(year % 100 === 0) {
+      if(year % 400 === 0) {
+        console.log('This is leap year!')
+      } else {
+        console.log('This is not a leap year!')
+      }
+    } else {
+      console.log('This is a leap year!')
+    }
+  } else {
+    console.log('This is not a leap year!')
+  }
+}
+
+isLeap(formatYearInNumber);
